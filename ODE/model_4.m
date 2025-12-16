76c7906784fcb4133905a0b3229fd8b22bd6bcea
silkyshin <77782287+silkyshin@users.noreply.github.com>
2025-12-16 14:24:14 +0000

    Trimer based nucleation model

---
+++ ODE/model_4.m
@@ -0,0 +1,36 @@
+function dMdt = odes_1to6(t, M, k)
+% ODE system for trimer based nucleation mode: 1 <-> 3 <-> 6
+%
+% Inputs:
+%   t - time (required by ode45)
+%   M - vector of concentrations: M = [M1; M3; M6]
+%   k - vector of rate constants:
+%       k = [k1, k_1, k2, k_2]
+%
+% Outputs:
+%   dMdt - derivative of each species
+
+% Unpack concentrations
+M1 = M(1);
+M3 = M(3);
+M6 = M(6);
+
+% Unpack rate constants
+k1  = k(1); k_1 = k(2);
+k2  = k(3); k_2 = k(4);
+
+% Stepwise ODEs
+
+% Monomer
+dM1 = -3*k1*M1^3 + 3*k_1*M3;
+
+% Trimer
+dM3 = 3*k1*M1^3 - k_1*M3 - 2*k2*M3^2 + 2*k_2*M6;
+
+% Hexamer
+dM6 = 2*k2*M3^2 - k_2*M6;
+
+% Return derivatives
+dMdt = [dM1; dM3; dM6];
+
+end
